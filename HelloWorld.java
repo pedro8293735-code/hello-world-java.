bb7a51ded558e087f29515399f901cc26e6fc1d5
pedro8293735-code <pedro.8293735@aluno.mg.gov.br>
2026-03-27 13:12:25 -0300

    Create  HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+public static void main(String[] args) {
+System.out.println("Olá, Mundo! Meu nome é [SEU NOME] e este é meu primeiro programa no GitHub.");
+System.out.println("Turma: [sua turma]");
+}
+}
